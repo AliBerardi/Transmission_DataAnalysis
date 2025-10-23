a2d207c2c165de3628ad168b6d48a626f3722d8e
Alice Berardi <208144553+AliBerardi@users.noreply.github.com>
2025-10-23 20:25:56 +0200

    add files

---
+++ EFFICIENCY_AllRuns.py
@@ -0,0 +1,210 @@
+########################################################################################################################################################################################################################################
+########################################################################################################################################################################################################################################
+# This program imports the module Histograms from the file Histograms_AllRuns.py
+# From the amplitude histograms imported it caluclates the efficiency.
+# The efficiency is the number of counts above an amplitude threshold (cut_a), normalized to the number of protons.
+# The efficiency values are plotted in a graph as a function of the run number, and then they are grouped in 2 histograms, separating the 2 sets of Sin or Sout, from which it is possible to extract the error associated to the efficiency.
+# In the efficiency graph, for each set are plotted a horizontal line, corresponding to the mean value, and two dashed lines at a distance of 2 standard deviations from the mean.
+# When calling the main function, it is possible to choose the detector (DET), select between Sample in and Sample out (run_type), choose the input file for which the
+# analysis need  to be performed, and finally choose the number of bins of the amplitude histograms.
+########################################################################################################################################################################################################################################
+########################################################################################################################################################################################################################################
+
+
+# import libraries
+import ROOT
+import numpy as np
+import math
+
+
+import configreader_cpp as cr
+from Histograms_AllRuns import Histograms
+
+
+def main(DET: int, run_type: str, cmnd_name: str, nbins: int):
+
+    # Import data from Histograms module
+    Histos_CUTamp, MAX_BIN, Entries, PI_TOT = Histograms(
+        DET, run_type, cmnd_name, nbins)
+
+    print("MAX_BIN", MAX_BIN)
+
+    LEN = len(Histos_CUTamp)
+
+    # Import data from cmnd file
+    cfg = cr.ConfigReader(cmnd_name)
+    Sin1 = cfg.get_int_vector("Sin1")
+    Sin2 = cfg.get_int_vector("Sin2")
+    Sout1 = cfg.get_int_vector("Sout1")
+    Sout2 = cfg.get_int_vector("Sout2")
+
+    if DET == 1:
+        Sin2 = cfg.get_int_vector("Sin2_DET1")
+    if DET == 8:
+        Sin1 = cfg.get_int_vector("Sin1_DET8")
+
+    if DET == 1:
+        cut_a = cfg.get_double("cut_a_det1")
+    if DET == 2:
+        cut_a = cfg.get_double("cut_a_det2")
+    if DET == 3:
+        cut_a = cfg.get_double("cut_a_det3")
+    if DET == 4:
+        cut_a = cfg.get_double("cut_a_det4")
+    if DET == 7:
+        cut_a = cfg.get_double("cut_a_det7")
+    if DET == 8:
+        cut_a = cfg.get_double("cut_a_det8")
+
+    print("---------------------------------------------------------------------")
+    print("Building efficiency graph")
+
+    #############################################################
+    # Calculate efficiency
+    Efficiency = []
+    Y_ERR = []
+    for j in range(0, LEN):
+        bin_cut = Histos_CUTamp[j].FindBin(cut_a)
+        bin_end = Histos_CUTamp[j].FindBin(45e+3)
+        eff = Histos_CUTamp[j].Integral(bin_cut, bin_end)
+        Efficiency.append(eff)
+
+        y_err = math.sqrt(Entries[j])/PI_TOT[j]
+        Y_ERR.append(y_err)
+
+    x_runs = np.arange(0, LEN, dtype='float64')
+    y_ratio = np.array(Efficiency, dtype='float64')
+    ex = np.zeros_like(x_runs, dtype='float64')  # No x errors
+    ey = np.array(Y_ERR, dtype='float64')
+
+    # check for NaN/Inf
+    finite = np.isfinite(y_ratio) & np.isfinite(ey)
+    x_runs, y_ratio, ex, ey = x_runs[finite], y_ratio[finite], ex[finite], ey[finite]
+    LEN = len(x_runs)
+
+    # divide in 2 colors
+    if run_type == "Sout":
+        n = len(Sout1)
+    if run_type == "Sin":
+        n = len(Sin1)
+    y_S1 = y_ratio[:n]
+    y_S2 = y_ratio[n:]
+    x_S1 = x_runs[:n]
+    x_S2 = x_runs[n:]
+    ey_S1 = ey[:n]
+    ey_S2 = ey[n:]
+    ex_S1 = ex[:n]
+    ex_S2 = ex[n:]
+
+    #############################################################
+    # Histo to extract error on efficiency
+    min = np.mean(y_ratio)-(0.5e-13)
+    max = np.mean(y_ratio)+(0.5e-13)
+    h_E1 = ROOT.TH1F("Efficiency histo 1", "Efficiency distribution detector " +
+                     str(DET) + " - " + str(run_type)+"1", 50, min, max)
+    h_E1.SetLineColor(ROOT.kBlue)
+    for e in y_S1:
+        h_E1.Fill(e)
+    mean1 = h_E1.GetMean()
+    sigma1 = h_E1.GetStdDev()
+    h_E1.GetXaxis().SetTitle("Efficiency (Counts / N protons)")
+    h_E1.GetYaxis().SetTitle("Entries")
+
+    h_E2 = ROOT.TH1F("Efficiency histo 2", "Efficiency distribution detector " +
+                     str(DET) + " - " + str(run_type)+"2", 50, min, max)
+    h_E2.SetLineColor(ROOT.kGreen+2)
+    for e2 in y_S2:
+        h_E2.Fill(e2)
+    mean2 = h_E2.GetMean()
+    sigma2 = h_E2.GetStdDev()
+    h_E2.GetXaxis().SetTitle("Efficiency (Counts / N protons)")
+    h_E2.GetYaxis().SetTitle("Entries")
+
+    # draw graph of efficiency
+    graph1 = ROOT.TGraphErrors(n, np.array(x_S1, dtype='float64'), np.array(
+        y_S1, dtype='float64'), np.array(ex_S1, dtype='float64'), np.array(ey_S1, dtype='float64'))
+    graph1.SetTitle(
+        f"Efficiency detector {DET} - {run_type}; Run; Efficiency (Counts / N protons)")
+    graph1.SetMarkerColor(ROOT.kBlue)
+    graph1.SetLineColor(ROOT.kBlue)
+    graph1.SetMarkerStyle(20)
+    n2 = len(x_S2)
+    graph2 = ROOT.TGraphErrors(n2, np.array(x_S2, dtype='float64'), np.array(
+        y_S2, dtype='float64'), np.array(ex_S2, dtype='float64'), np.array(ey_S2, dtype='float64'))
+    graph2.SetMarkerColor(ROOT.kGreen+2)
+    graph2.SetLineColor(ROOT.kGreen+2)
+    graph2.SetMarkerStyle(20)
+    GRAPH = ROOT.TMultiGraph()
+    GRAPH.Add(graph1)
+    GRAPH.Add(graph2)
+
+    line1 = ROOT.TLine(0, mean1, n, mean1)
+    line1.SetLineColor(ROOT.kRed)
+    line1.SetLineWidth(2)
+    line1u = ROOT.TLine(0, (mean1+(2*sigma1)), n, (mean1+(2*sigma1)))
+    line1u.SetLineColor(ROOT.kRed)
+    line1u.SetLineStyle(2)
+    line1d = ROOT.TLine(0, (mean1-(2*sigma1)), n, (mean1-(2*sigma1)))
+    line1d.SetLineColor(ROOT.kRed)
+    line1d.SetLineStyle(2)
+
+    line2 = ROOT.TLine(n, mean2, LEN, mean2)
+    line2.SetLineColor(ROOT.kRed)
+    line2.SetLineWidth(2)
+    line2u = ROOT.TLine(n, (mean2+(2*sigma2)), LEN, (mean2+(2*sigma2)))
+    line2u.SetLineColor(ROOT.kRed)
+    line2u.SetLineStyle(2)
+    line2d = ROOT.TLine(n, (mean2-(2*sigma2)), LEN, (mean2-(2*sigma2)))
+    line2d.SetLineColor(ROOT.kRed)
+    line2d.SetLineStyle(2)
+
+    c_r2 = ROOT.TCanvas("Ratio", "Canvas", 1200, 750)
+    c_r2.SetLeftMargin(0.15)
+    GRAPH.Draw("APL")
+    GRAPH.SetTitle(
+        f"Efficiency detector {DET} - {run_type}; Run; Efficiency (Counts / N protons)")
+    GRAPH.GetXaxis().SetLimits(0, LEN)
+    line1.Draw("same")
+    line1u.Draw("same")
+    line1d.Draw("same")
+    line2.Draw("same")
+    line2u.Draw("same")
+    line2d.Draw("same")
+    c_r2.cd()
+    pave = ROOT.TPaveText(0.74, 0.75, 0.96, 0.87, "NDC")
+    pave.AddText(f"Threshold = {int(cut_a)} channels")
+    pave.SetTextAlign(12)
+    pave.SetTextSize(0.025)
+    pave.SetTextFont(42)
+    pave.SetBorderSize(1)
+    pave.SetFillStyle(1001)
+    pave.SetFillColor(ROOT.kWhite)
+    pave.Draw()
+    c_r2.Update()
+    c_r2.SaveAs(f"./OUTPUT/Efficiency/Efficiency_singleruns_det{DET}_{run_type}.png")
+
+    # Draw error graph
+    c_E = ROOT.TCanvas("E", "Canvas", 1200, 750)
+    c_E.Divide(2, 1)
+    c_E.cd(1)
+    h_E1.Draw("HIST")
+    c_E.cd(2)
+    h_E2.Draw("HIST")
+    c_E.cd()
+    pave = ROOT.TPaveText(0.70, 0.65, 0.99, 0.77, "NDC")
+    pave.AddText(f"Threshold = {int(cut_a)} channels")
+    pave.SetTextAlign(12)
+    pave.SetTextSize(0.023)
+    pave.SetTextFont(42)
+    pave.SetBorderSize(1)
+    pave.SetFillStyle(1001)
+    pave.SetFillColor(ROOT.kWhite)
+    pave.Draw()
+    c_E.Update()
+    c_E.SaveAs(
+        f"./OUTPUT/Efficiency/EfficiencyHistogram_singleruns_det{DET}_{run_type}.png")
+
+
+if __name__ == "__main__":
+    main(DET=2, run_type="Sout",
+         cmnd_name="./input_files/Histograms_AllRuns.cmnd", nbins=300)
---
+++ Histograms_AllRuns.py
@@ -0,0 +1,206 @@
+########################################################################################################################################################################
+#  This program analyses single runs and for each creates the amplitude spectra, normalized to the number of protons.
+#  It is organized in functions.
+#  The first function in this program builds the data path reading input from a cmnd file.
+#  The next function builds the dataframe for the detector of interest.
+#  The following function reads the PKUP detector tree and calculates the pulse intensity of each run, which will be used to normalize the histograms.
+#  The next function builds the amplitude histograms with cuts. It returns a tuple with the list of all the amplitude histograms
+#  (one for each run), the list of the position of the bin with the maximum amplitude, the number of entries and the pulse intensity of each run.
+#  There is a last function which can be used to plot the full amplitude histogram of a few runs. For example if, from the general plot, a strange behaviour is observed,
+#  this can be useful to analyze in detail the problematic runs.
+########################################################################################################################################################################
+
+# import libraries
+import ROOT
+import numpy as np
+import pandas as pd
+import math
+from array import array
+from datetime import datetime
+import sys
+import os
+import ast
+
+import configreader_cpp as cr
+ROOT.ROOT.EnableImplicitMT()
+
+
+def data(DET: int, run_type: str, cmnd_name: str):
+
+    cfg = cr.ConfigReader(cmnd_name)
+    Sin = cfg.get_int_vector("Sin")
+    Sout = cfg.get_int_vector("Sout")
+
+    if DET == 1:
+        Sin = cfg.get_int_vector("Sin_DET1")
+    if DET == 8:
+        Sin = cfg.get_int_vector("Sin_DET8")
+
+    if run_type == "Sin":
+        runlist = Sin
+    if run_type == "Sout":
+        runlist = Sout
+
+    prefix = cfg.get_string("prefix")
+    suffix = cfg.get_string("suffix")
+
+    filelist = [prefix + str(run) + suffix for run in runlist]
+
+    return (filelist, runlist)
+
+
+# Build dataframe for the detector of interest
+def dataframe(DET: int, run_type: str, cmnd_name: str):
+
+    filelist = data(DET, run_type, cmnd_name)[0]
+
+    # I create a list which I fill with dataframes corresponding to my detector (FC-U) for each run
+    DF_det = []
+    for file in filelist:
+        df = ROOT.RDataFrame("FC-U", file)
+        # filter dataframe for the dector of interest
+        df = df.Filter(f'detn=={DET}')
+        DF_det.append(df)
+    return (DF_det)
+
+
+# extract Pulse Intensity from PKUP
+def PulseInt(DET: int, run_type: str, cmnd_name: str):
+
+    filelist = data(DET, run_type, cmnd_name)[0]
+
+    DF_PK_S = []
+    for file in filelist:
+        df_p = ROOT.RDataFrame("PKUP", file)
+        DF_PK_S.append(df_p)
+
+    d_PI = {}
+    PI = {}
+    PI_TOT = {}
+    for k in range(0, len(DF_PK_S)):
+        arr = DF_PK_S[k].AsNumpy(["PulseIntensity"])["PulseIntensity"]
+        PI_TOT[k] = float(np.sum(arr, dtype=np.float64))
+    return (PI_TOT)
+
+
+# Create the amplitude histograms
+def Histograms(DET: int, run_type: str, cmnd_name: str, nbins: int):
+
+    runlist = data(DET, run_type, cmnd_name)[1]
+    DF_det = dataframe(DET, run_type, cmnd_name)
+    PI_TOT = PulseInt(DET, run_type, cmnd_name)
+
+    cfg = cr.ConfigReader(cmnd_name)
+
+    if DET == 1:
+        cut_a = cfg.get_double("cut_a_det1")
+    if DET == 2:
+        cut_a = cfg.get_double("cut_a_det2")
+    if DET == 3:
+        cut_a = cfg.get_double("cut_a_det3")
+    if DET == 4:
+        cut_a = cfg.get_double("cut_a_det4")
+    if DET == 7:
+        cut_a = cfg.get_double("cut_a_det7")
+    if DET == 8:
+        cut_a = cfg.get_double("cut_a_det8")
+
+    print("Building amplitude histograms")
+    print("For DETECTOR: ", DET)
+    print("With amplitude threshold: ", int(cut_a))
+    print(f"Processing {run_type} runs: \n", runlist)
+
+    # define dictionaries to store the amplitude values
+    d_Amp = {}
+    Amp = {}
+    # define lists to store the histograms, the number of entries and the x position of the maximum bin
+
+    Histos_CUTamp = []
+    Entries = []
+    MAX_BIN = []
+
+    for i in range(0, len(DF_det)):  # loop over the datframes for the different runs
+
+        # For each run, I extract all the amplitudes from the corresponding dataframe and put them in a dictionary
+        d_Amp[i] = DF_det[i].AsNumpy(["amp"])
+        Amp[i] = d_Amp[i]["amp"]
+        Amplitude = np.array(Amp[i])  # convert the list into a numpy array
+
+        # histogram with cut on amplitude
+        Amplitude_cut = Amplitude[Amplitude > cut_a]
+        histo_cut = ROOT.TH1F(
+            f"Amplitude_histo_cut_{i}", f"Amplitudes detector {DET} with cut - {run_type}", nbins, 0, 45.e+3)
+        for j in Amplitude_cut:
+            histo_cut.Fill(j)
+
+        histo_cut.GetXaxis().SetTitle("Amplitude (channels)")
+        histo_cut.GetYaxis().SetTitle("Entries / N protons")
+        Entries.append(int(Amplitude_cut.size))
+
+        histo_cut.Scale(1/PI_TOT[i])
+
+        Histos_CUTamp.append(histo_cut)
+
+        # Get the position of the maximum bin and store it in a list
+        max_bin = histo_cut.GetMaximumBin()
+        x_max = histo_cut.GetBinCenter(max_bin)
+        MAX_BIN.append(x_max)
+
+    return (Histos_CUTamp, MAX_BIN, Entries, PI_TOT)
+
+
+# You can call this function if you want to draw the amplitudes of some runs.
+
+def Plot(DET: int, run_type: str, cmnd_name: str, nbins: int, first_run_plot: int, last_run_plot: int):
+
+    runlist = data(DET, run_type, cmnd_name)[1]
+    DF_det = dataframe(DET, run_type, cmnd_name)
+    PI_TOT = PulseInt(DET, run_type, cmnd_name)
+
+    print("---------------------------------------------------------------------")
+    print(
+        f"Plotting full amplitude histograms of runs from {first_run_plot} to {last_run_plot-1}")
+
+    d_Amp = {}
+    Amp = {}
+    Histos = []
+
+    for i in range(0, len(DF_det)):
+        # For each run, extract all the amplitudes from the corresponding dataframe and put them in a dictionary
+        d_Amp[i] = DF_det[i].AsNumpy(["amp"])
+        Amp[i] = d_Amp[i]["amp"]
+        Amplitude = np.array(Amp[i])  # convert the list into a numpy array
+
+        # Define an histogram and fill it with the amplitudes
+        histo = ROOT.TH1F(
+            f"Amplitude_histo_{i}", f"Amplitudes detector {DET} - {run_type}", nbins, 0, 45.e+3)
+        for x in Amplitude:
+            histo.Fill(x)
+
+        histo.GetXaxis().SetTitle("Amplitude (channels)")
+        histo.GetYaxis().SetTitle("Entries / N protons")
+
+        histo.Scale(1/PI_TOT[i])  # divide by proton number
+
+        Histos.append(histo)
+
+    # Draw histogram of amplitudes
+
+    colors = [ROOT.kBlue, 4, 6, 7, 8, 9, 28, 30, 32, 34, 36, 38, 40, 41, 42, 44, 46, 48, 50, 52,
+              55, 57, 58, 60, 62, 64, 65, 67, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90]  # 2
+    legend = ROOT.TLegend(0.6, 0.38, 0.85, 0.85)
+    c_CUT = ROOT.TCanvas('Amplitudes', 'stability amp CUTS', 1200, 750)
+    ROOT.gStyle.SetOptStat(0)
+    for hc, i in zip(range(first_run_plot, last_run_plot), range(0, (last_run_plot-first_run_plot))):
+        Histos[hc].SetLineColor(colors[i])
+        Histos[hc].Draw("HIST same")
+        legend.AddEntry(Histos[hc], f"run 121{runlist[hc]} : {hc}", "l")
+    legend.Draw()
+    c_CUT.SaveAs(
+        f"./OUTPUT/Amplitudes_det{DET}_{run_type}_from{first_run_plot}_to{last_run_plot-1}.png")
+
+
+if __name__ == "__main__":
+    
+    Plot(DET=3, run_type="Sin", cmnd_name="./input_files/Histograms_AllRuns.cmnd",
+         nbins=150, first_run_plot=5, last_run_plot=11)
